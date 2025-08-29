5e4ec6d8b37230166a6a7253e6dc2b0252265469
Nick Kesseru <kesserumini@Nicks-Mac-mini.local>
2025-08-29 07:01:34 -0700

    chore: remove SSR test page to stop / crash; keep top-level pages/api

--- pages/ssr-test.tsx
+++
@@ -1,7 +0,0 @@
-export async function getServerSideProps() {
-  const res = await fetch('/api/ping'); // relative path
-  return { props: { ok: res.ok } };
-}
-export default function SSRTest({ ok }: { ok: boolean }) {
-  return <pre>{JSON.stringify({ ok })}</pre>;
-}
